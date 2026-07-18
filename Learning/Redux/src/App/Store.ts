17ffc12e6cfffb4d42ac14611c9218b02296722a
imamanjangra <imamanjangra@gmail.com>
2026-07-18 16:02:27 +0530

    Learning redux

---
+++ Learning/Redux/src/App/Store.ts
@@ -0,0 +1,59 @@
+import { configureStore } from '@reduxjs/toolkit';
+import CounterReducer from '../Features/Counter/CounterSlice.ts';
+
+export const store = configureStore({
+    reducer : {
+        counter : CounterReducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+
+/* 
+configureStore 
+    -> it create the store for the application 
+
+import counterReducer 
+    -> it import the logial part of the store
+
+export store 
+    -> we export this to import in main.tsx file beacuse it is the main file 
+    -> we import it into main and wrap like a provider to whole application 
+
+
+Store
+
+    -> The Store is the single source of truth.
+
+    -> It contains all global application state.
+
+    Example
+
+    {
+        counter:{
+            value:0
+        },
+
+        auth:{
+            user:null
+        },
+
+        theme:{
+            theme:"light"
+        }
+    }
+
+  reducer
+        -> Combines all reducers (slices) into one Redux Store.
+        -> Every key becomes a property inside the Redux state.
+        -> Left side  = State key
+        -> Right side = Reducer function
+
+        Example
+
+        reducer:{
+            counter: counterReducer
+        }
+*/
---
+++ Learning/Redux/src/Features/Counter/CounterSlice.ts
@@ -0,0 +1,80 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+
+
+interface CounterState {
+    value : number;
+}
+
+const initialState : CounterState = {
+    value : 0,
+}
+
+const CounterSlice =  createSlice({
+    name : "counter",
+    initialState,
+    reducers : {
+        increment(state) {
+            state.value++;
+        },
+
+        decrement(state){
+            state.value--
+        },
+
+        incrementByAmount(state , action : PayloadAction<number>){
+            state.value += action.payload;
+        },
+
+        reset(state){
+            state.value = 0;
+        }
+    }
+})
+
+export const {
+    increment , 
+    decrement, 
+    incrementByAmount,
+    reset
+} = CounterSlice.actions;
+
+export default CounterSlice.reducer;
+
+
+/*
+Interface 
+    -> Defines the shape (type) of the slice 
+    -> TypeScript uses it for type checking
+
+InitialState 
+    -> Defines the initial state of this slice
+    -> Redux uses it when the store is created
+    -> Without it, Redux not know the initial value of this slice 
+
+CreateSlice 
+    -> it come with redux toolkit to create a slice 
+    -> it need a three (3) items 
+        1) name 
+        2) initialvalue 
+        3) reducers
+
+Reducers 
+    -> Contains functions that describe how the state should change
+    -> it give two option 
+        1)state
+            -> Redux automatically passes the current slice state
+            -> We use it to read or update the state
+
+        2)action
+            -> Redux automatically creates this object.
+            -> It contains:
+                type ->  counter/incrementByAmount somthing like this
+               payload
+                     -> Additional data sent with an action.
+                     -> Example:
+                                 dispatch(incrementByAmount(10))
+                                 payload = 10
+
+
+   
+*/
